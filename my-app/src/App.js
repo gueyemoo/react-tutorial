cd1962b4d0e16cb0b941400ca407c588560d7a67
gobelait <antoine.db99@gmail.com>
2021-10-21 14:03:03 +0200

    ajout login component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Login from "./component/Login";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
     <div className="App">
         <h1>HELLO WORLD</h1>
+
+        <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Login />
+              </Route>
+            </Switch>
+          </div>
+
     </div>
+
   );
 }
 
